2e0eaec5ac327d650bbffae7e3080e2c3b53e27f
GIOVANNI CLEMENT <20013287@M100324.hq.corp.leroymerlin.com>
2020-10-27 22:59:38 +0100

    initial commi

---
+++ README.md
@@ -0,0 +1,26 @@
+# Nav Menu Roles + YITH WooCommerce Memberships  
+
+A bridge plugin that adds YITH WooCommerce Membership Plans to Nav Menu Roles menu options. 
+It is adapted from helgatheviking plugin, working only for Woo Commerce Membership plugin.
+
+## Requirements
+1. [Nav Menu Roles](https://wordpress.org/plugins/nav-menu-roles/)
+2. [WooCommerce Memberships](https://yithemes.com/themes/plugins/yith-woocommerce-membership/)
+3. Nav Menu Roles + YITH WooCommerce Membership
+
+All three plugins must be installed, and activated in order to get the desired result. 
+
+## Usage
+
+1. Go to Appearance > Menus
+1. Set the "Display Mode" to "Logged In Users"
+1. Under "Restrict menu item to a minimum role" you will now see all your WooCommerce Memberships
+1. Check the boxes next to the desired memberships
+
+![Show the Membership Plans](assets/screenshot-1.png)
+
+## Credtis & Donate 
+
+Absolutly don't care about rewards. 
+Please donate to the initial idea from : [helgatheviking-Woocommerce-membership](https://github.com/helgatheviking/nav-menu-roles-woocommerce-memberships) , first implemented for Woo Commerce Membership plugin.
+
---
+++ nav-menu-roles-woocommerce-memberships.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * Plugin Name:       Nav Menu Roles + WooCommerce Memberships Bridge
+ * Plugin URI:        http://github.com/helgatheviking/nav-menu-roles-woocommerce-memberships
+ * Description:       Add WooCommerce Membership Plans to Nav Menu Roles
+ * Version:           1.0.0
+ * Author:            Kathy Darling
+ * Author URI:        http://kathyisawesome.com
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * GitHub Plugin URI: helgatheviking/nav-menu-roles-woocommerce-memberships
+ * GitHub Branch: master
+ *
+ * @package Nav Menu Role WooCommerce Memberships Bridge
+ * @category Core
+ * @author Kathy Darling
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/*
+ * Add NMR filters if both plugins are active
+ * @since 1.0.0
+ */
+function nmr_wcm_init(){
+    if ( defined( 'YITH_WCMBS_VERSION' ) ){
+		add_filter( 'nav_menu_roles', 'nmw_yith_wcm_new_roles' );
+		add_filter( 'nav_menu_roles_item_visibility', 'nmw_yith_wcm_item_visibility', 10, 2 );
+    }
+}
+add_action( 'plugins_loaded', 'nmr_wcm_init', 20 );
+
+
+/*
+ * Add custom roles to Nav Menu Roles menu options
+ *
+ * @param array $roles An array of all available roles, by default is global $wp_roles
+ * @return array
+ * @since 1.0.0
+ */
+function nmw_yith_wcm_new_roles( $roles ){
+	return array_merge( $roles, nmr_yith_wcm_get_roles_wrapper() );
+}
+
+
+/*
+ * Change visibilty of each menu item
+ * NMR settings can be "in" (all logged in), "out" (all logged out) or an array of specific roles
+ *
+ * @param bool $visible
+ * @param object $item The menu item object. Nav Menu Roles adds its info to $item->roles
+ * @return boolean
+ * @since 1.0.0
+ */
+function nmw_yith_wcm_item_visibility( $visible, $item ){
+
+  if( ! $visible && isset( $item->roles ) && is_array( $item->roles ) ){
+
+        // Get the plugin-specific roles for this menu item.
+        $roles = nmr_wcm_get_relevant_roles_wrapper( $item->roles );
+
+        if( count( $roles ) > 0 ) {
+
+            // Only need to look through the relevant roles.
+            foreach( $roles as $role ) {
+               
+                // Test if the current user has the specific plan membership.
+                if ( nmr_wcm_current_user_can_wrapper( $role ) ){
+                    $visible = true;
+                    break;
+                } else {
+                    $visible = false;
+                }
+            }
+
+        }
+
+    }
+
+    return $visible;
+
+}
+
+/*-----------------------------------------------------------------------------------*/
+/* Helper Functions */
+/*-----------------------------------------------------------------------------------*/
+
+/*
+ * Get the plugin-specific "roles", from YITH Woo Commerce plugin, returned in an array, with ID => Name key pairs
+ *
+ * @return array
+ * @since 1.1.0
+ */
+function nmr_yith_wcm_get_roles_wrapper(){
+    $roles = array();
+
+    $plans = YITH_WCMBS_Manager()->get_plans();
+    
+    if( ! empty( $plans ) ) {
+        foreach( $plans as $plan ){
+            $roles['wc_membership_' . $plan->ID] = $plan->post_title;
+        }
+    }
+    
+    return $roles;
+}
+
+/*
+ * Get the plugin-specific "roles" relevant to this menu item
+ *
+ * @return array
+ * @since 1.1.0
+ */
+function nmr_wcm_get_relevant_roles_wrapper( $roles = array() ){
+    return preg_grep( '/^wc_membership_*/', $roles );
+}
+
+/*
+ * Check the current user has plugin-specific level capability
+ *
+ * @param string $role_id | The ID of the "role" with a plugin-specific prefix
+ * @return bool
+ * @since 1.1.0
+ */
+function nmr_wcm_current_user_can_wrapper( $role_id = false ) {
+
+    $user_id = get_current_user_id();
+
+    if( ! $user_id || ! $role_id ) {
+        return false;
+    }
+
+    $role_id = str_replace( 'wc_membership_', '', $role_id );
+    return YITH_WCMBS_Manager()->get_allowed_posts_in_plan( $role_id, true );
+}
